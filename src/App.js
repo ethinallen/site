c9692efff81affa83eae8f223f73f8ad716e653d
Emery <abemery@clemson.edu>
2020-09-12 22:31:06 -0400

    not much but it's something

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Typing from 'react-typing-animation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typing speed={50}>
+          <h1>My name is Andrew Emery.</h1>
+          <Typing.Delay ms={1000} />
+          <Typing.Backspace count={25} />
+          <h1>I graduate May 2021.</h1>
+          <Typing.Delay ms={1000} />
+          <Typing.Backspace count={25} />
+          <h1>I am seeking full-time cloud consulting positions.</h1>
+          <Typing.Delay ms={1000} />
+          <Typing.Backspace count={50} />
+          <h1>Download my resume for my contact information.</h1>
+          <Typing.Delay ms={1000} />
+        </Typing>
       </header>
     </div>
   );
 }
+
+
 
 export default App;
