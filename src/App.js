681fc81b78cba58e8e9944a08e175530283394ac
Emery <abemery@clemson.edu>
2020-09-15 10:15:54 -0400

    updating my super shite website :))

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Typing from 'react-typing-animation';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Typing speed={50}>
-          <h1>My name is Andrew Emery.</h1>
-          <Typing.Delay ms={1000} />
-          <Typing.Backspace count={25} />
-          <h1>I graduate May 2021.</h1>
-          <Typing.Delay ms={1000} />
-          <Typing.Backspace count={25} />
-          <h1>I am seeking full-time cloud consulting positions.</h1>
-          <Typing.Delay ms={1000} />
-          <Typing.Backspace count={50} />
-          <h1>Download my resume for my contact information.</h1>
-          <Typing.Delay ms={1000} />
-        </Typing>
-      </header>
+      <div className="bkg_containter">
+        <header className="App-header">
+          <Typing speed={75}>
+            <h1>My name is Drew Emery.</h1>
+            <Typing.Delay ms={1000} />
+            <Typing.Backspace count={25} />
+            <h1>I graduate May 2021.</h1>
+            <Typing.Delay ms={1000} />
+            <Typing.Backspace count={25} />
+            <h1>I am seeking full time-cloud consultant positions.</h1>
+            <Typing.Delay ms={1000} />
+            <Typing.Backspace count={50} />
+            <h1>Download my <a href="./files/drewEmeryResume.pdf" download>resume</a> for my contact information.</h1>
+            <Typing.Delay ms={1000} />
+          </Typing>
+        </header>
+      </div>
     </div>
   );
 }
