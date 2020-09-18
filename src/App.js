b7fff512aaa2a5cc07a26480a6e89ff2e8b85fc3
Emery <abemery@clemson.edu>
2020-09-18 15:52:42 -0400

    i think this works now?

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import React from 'react';
 import './App.css';
-import Typing from 'react-typing-animation';
-
+import { ParallaxProvider } from 'react-scroll-parallax';
+import Sidebar from './components/Sidebar'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <div className="bkg_containter">
-        <header className="App-header">
-          <Typing speed={75}>
-            <h1>My name is Drew Emery.</h1>
-            <Typing.Delay ms={1000} />
-            <Typing.Backspace count={25} />
-            <h1>I graduate May 2021.</h1>
-            <Typing.Delay ms={1000} />
-            <Typing.Backspace count={25} />
-            <h1>I am seeking full time-cloud consultant positions.</h1>
-            <Typing.Delay ms={1000} />
-            <Typing.Backspace count={50} />
-            <h1>Download my <a href="./files/drewEmeryResume.pdf" download>resume</a> for my contact information.</h1>
-            <Typing.Delay ms={1000} />
-          </Typing>
-        </header>
-      </div>
+
+      <Sidebar width={175} height={"100vh"}>
+        <h1> Drew. </h1>
+      </Sidebar>
+
+      <Header/>
+
     </div>
   );
 }
-
-
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Typing from 'react-typing-animation';
+
+const Header = () => (
+  <header className="App-header">
+    <Typing speed={75}>
+      <h1>My name is Drew Emery.</h1>
+      <Typing.Delay ms={1000} />
+      <Typing.Backspace count={25} />
+      <h1>I am graduating May 2021.</h1>
+      <Typing.Delay ms={1000} />
+      <Typing.Backspace count={25} />
+      <h1>I am seeking full time-cloud consultant positions.</h1>
+      <Typing.Delay ms={1000} />
+      <Typing.Backspace count={50} />
+      <h1>Download my <a href="./files/drewEmeryResume.pdf" download>resume</a> for my contact information.</h1>
+    </Typing>
+  </header>
+)
+
+export default Header;
