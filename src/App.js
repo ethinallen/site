925922dabdd625056e3f916da2776107b5d78927
Emery <abemery@clemson.edu>
2020-09-28 16:23:40 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import './App.css';
 import { ParallaxProvider } from 'react-scroll-parallax';
-import Sidebar from './components/Sidebar'
-import Header from './components/Header'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
 
 function App() {
   return (
+
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+
     <div className="App">
 
       <Sidebar width={175} height={"100vh"}>
-        <h1> Drew. </h1>
+        <h1> Drew.
+        <Text style={{color: 'blue'}}
+              onPress={() => Linking.openURL('http://google.com')}>
+          Google
+        </Text>
+        </h1>
+        <href=
       </Sidebar>
 
       <Header/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const Header = () => (
       <h1>I am seeking full time-cloud consultant positions.</h1>
       <Typing.Delay ms={1000} />
       <Typing.Backspace count={50} />
-      <h1>Download my <a href="/files/drewEmeryResume.pdf" target="_blank" download>resume</a> for my contact information.</h1>
+      <h1>Emai me at abemery@clemson.edu</h1>
     </Typing>
   </header>
 )
