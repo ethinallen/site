33c63b84479c1dc6457e04862f9f0ab8b1969d18
Emery <abemery@clemson.edu>
2020-10-06 14:07:08 -0500

    background

--- src/App.js
+++ src/App.js
@@ -8,22 +8,10 @@ import Header from './components/Header';
 function App() {
   return (
 
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </Router>
-
     <div className="App">
 
       <Sidebar width={175} height={"100vh"}>
-        <h1> Drew.
-        <Text style={{color: 'blue'}}
-              onPress={() => Linking.openURL('http://google.com')}>
-          Google
-        </Text>
-        </h1>
-        <href=
+
       </Sidebar>
 
       <Header/>
