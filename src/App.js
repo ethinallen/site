8d9148241075d055b901edec69f8629179aaad9d
Emery <abemery@clemson.edu>
2020-10-17 11:11:28 -0400

    jfc

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import './components/Van.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from "./components/Home.js"
-import Login from "./components/Login.js"
 import Van from "./components/Van.js"
 
 
@@ -13,7 +12,6 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
         <Route path="/van" component={Van} />
       </Switch>
     </Router>
