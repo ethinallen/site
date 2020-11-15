e50aa76da471f7f4d07640deb79b7c1f4c8476f9
Emery <abemery@clemson.edu>
2020-11-15 00:50:00 -0500

    i'm triple dumb because i spelled it 'placeholer' this entire time

--- src/components/Gitter.js
+++ src/components/Gitter.js
@@ -1,41 +1,36 @@
 import React, { Component } from 'react';
 import './Gitter.css'
 import Geets from './Geets'
-import SearchBar from './SearchBar'
 import Particles from 'react-particles-js'
 import Typing from 'react-typing-animation'
 
-class Gitter extends Component {
-  constructor(props) {
-        super(props);
-        this.state = {id: '',
-                      url: 'https://7vcvnq8z2g.execute-api.us-east-2.amazonaws.com/staging/fetch/',
-                      data: {}
-                    }
+function Gitter() {
+  // const [] = useState([]);
 
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({id: event.target.value});
-    }
-
-    handleSubmit(event) {
-        event.preventDefault()
-        fetch(this.state.url + this.state.id)
-        .then(response => response.json())
-        .then(responseData => {
-            console.log(responseData)
-            this.setState({
-                data:responseData
-            })
-        })
-    }
-
-  render() {
-
+    //     this.state = {id: '',
+    //                   url: 'https://7vcvnq8z2g.execute-api.us-east-2.amazonaws.com/staging/fetch/',
+    //                   data: {'testing':}
+    //                 }
+    //
+    //
+    //     this.handleChange = this.handleChange.bind(this);
+    //     this.handleSubmit = this.handleSubmit.bind(this);
+    //
+    // handleChange(event) {
+    //     this.setState({id: event.target.value});
+    // }
+    //
+    // handleSubmit(event) {
+    //     event.preventDefault()
+    //     fetch(this.state.url + this.state.id)
+    //     .then(response => response.json())
+    //     .then(responseData => {
+    //         this.setState({
+    //             data: responseData['response']
+    //         })
+    //     })
+    //     .then(console.log(this.state.data))
+    // }
     return (
       <div id="gitter-body">
 
@@ -48,9 +43,9 @@ class Gitter extends Component {
           </div>
         </div>
 
-        <form className="ui large form" onSubmit={this.handleSubmit}>
+        <form className="ui large form" >
             <div className="field">
-                <input type="text" placeholer="discover user geets" value={this.state.id} onChange={this.handleChange} required/>
+                <input type="text" placeholder="discover geets" required/>
             </div>
         </form>
 
@@ -78,7 +73,6 @@ class Gitter extends Component {
 
       </div>
     );
-  }
 }
 
 export default Gitter;
