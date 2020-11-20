fb688dd756ba055b83d9f47e6f3a4053c13322e9
Drew <abemery@clemson.edu>
2020-11-20 11:08:45 -0500

    ga initialized

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import "semantic-ui-css/semantic.min.css";
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('G-KN41TN8D9F', {
+  debug: true,
+  titleCase: false,
+  gaOptions: {
+    userId: 420
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
