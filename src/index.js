d07127ad5bdbf2623b9811c8aec5c906643be325
Drew <abemery@clemson.edu>
2020-11-20 11:19:24 -0500

    remove debug

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import './index.css';
 import App from './App';
 import "semantic-ui-css/semantic.min.css";
 
-ReactGA.initialize('G-KN41TN8D9F', {
-  debug: true,
-  titleCase: false});
+ReactGA.initialize('G-KN41TN8D9F');
 
 ReactDOM.render(
   <React.StrictMode>
