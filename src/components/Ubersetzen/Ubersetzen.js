e74f65280aac898d0ec069e7f57de0e7abe3a6c0
Drew <abemery@clemson.edu>
2021-04-15 14:28:40 -0400

    loading bar, still waiting on ssl but cleaning up styling in the meantime; need to handle lyric fetchng exceptions

--- src/components/Ubersetzen/Ubersetzen.js
+++ src/components/Ubersetzen/Ubersetzen.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import { Card, Grid, Image } from 'semantic-ui-react';
-
-
-import { authEndpoint, clientId, redirectUri, scopes } from "./config";
-import hash from "./hash";
 import queryString from 'query-string';
+import ReactLoading from 'react-loading';
+
 import './Ubersetzen.css';
 
 class Ubersetzen extends Component {
@@ -12,7 +10,8 @@ class Ubersetzen extends Component {
     super(props);
     this.state = {
       isAuthenticatedWithSpotify: false,
-      serverData: {}
+      serverData: {},
+      lyrics : {}
     };
   }
 
@@ -44,63 +43,60 @@ class Ubersetzen extends Component {
       fetch('https://arbeiter.xyz:8888/lyrics/' + this.state.serverData.artist + '/' + this.state.serverData.songTitle)
       .then(response => response.json())
       .then((data) => this.setState({
-        serverData: {
+        lyrics: {
           native_lyrics: data['lyrics']
         }
       }))
     )
-
-
-
   }
 
 
   render() {
 
     return (
-      <div className="Home">
-          <div id="contentContainer">
-          <div id="cardContainer">
-            {this.state.user ?
-                  <Card>
-                    <Card.Content>
-                      <Image
-                      floated='right'
-                      size='large'
-                      src={this.state.serverData.imageURL}
-                      />
-                      <Card.Header>{this.state.serverData.songTitle}</Card.Header>
-                      <Card.Meta>{this.state.serverData.artist}</Card.Meta>
-                    </Card.Content>
-                  </Card>
+        <div className="contentContainer">
+
+          <div className="contentColumn">
+          <div id="header">Ubersetzen.</div>
+
+            <div id="cardContainer">
+              {
+                this.state.user ?
+                    <Card>
+                      <Card.Content>
+                        <Image
+                        floated='right'
+                        size='large'
+                        src={this.state.serverData.imageURL}
+                        />
+                        <Card.Header>{this.state.serverData.songTitle}</Card.Header>
+                        <Card.Meta>{this.state.serverData.artist}</Card.Meta>
+                      </Card.Content>
+                    </Card>
 
 
-              : <div className="signInButton" onClick={() => {
-                window.location = window.location.href.includes('localhost')
-                  ? 'https://localhost:8888/login'
-                  : 'https://arbeiter.xyz:8888/login' }
-                }
-                >Sign in with Spotify</div>
-              }
-            </div>
+                : <div className="signInButton" onClick={() => {
+                  window.location = window.location.href.includes('localhost')
+                    ? 'http://localhost:8888/login'
+                    : 'https://arbeiter.xyz:8888/login' }
+                  }
+                  >Sign in with Spotify</div>
+            }
+          </div>
 
-            <div id="lyricsContainer">
-              <div id="lyric-container">
-                {
-                  this.state.serverData.native_lyrics ?
-                  <div id="lyrics">
-                  {this.state.serverData.native_lyrics.split("\n").map((i,key) => {
-            return <div key={key}>{i}</div>;
-        })}
-                  </div>
-
-                  : <div>waiting on lyrics... </div>
-                }
+          <div id="lyric-container">
+            {
+              this.state.lyrics.native_lyrics ?
+                <div id="lyrics">
+                  {this.state.lyrics.native_lyrics.split("\n").map((i,key) => {
+                  return <div key={key}>{i}</div> ;
+                })}
               </div>
 
-            </div>
+              : <div><ReactLoading type="cylon" color="#fff" />waiting on lyrics... </div>
+            }
+          </div>
         </div>
-
       </div>
     );
   }
